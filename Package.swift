87d0cac18ba04c6814b4761f76b01e7e3613ee45
Daniel Ahlborn <78955918+iwt-dahlborn@users.noreply.github.com>
2022-09-22 15:24:40 +0200

    feat(MXOM-84): added spm support (#1)

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iwb_ios_MWPhotoBrowser",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "MWPhotoBrowser",
+            targets: ["MWPhotoBrowser"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.0.0"),
+        .package(url: "https://github.com/matej/MBProgressHUD.git", from: "1.2.0")
+    ],
+    targets: [
+        .target(
+            name: "MWPhotoBrowser",
+            dependencies: [
+                "SDWebImage",
+                "MBProgressHUD"
+            ],
+            resources: [
+                .process("./Resources/")
+            ]
+        )
+    ]
+)
